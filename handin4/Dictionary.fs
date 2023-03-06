884eb21cdcd6529846d39c213d86f1044d34d9d7
LF <lufr@itu.dk>
2023-03-06 13:39:49 +0100

    added dictionary assignment

--- handin4/Dictionary.fs
+++ handin4/Dictionary.fs
@@ -1,5 +1,5 @@
 module Dictionary
     type Dictionary = D of Set<string>
     let empty () = D Set.empty<string>
-    let insert (s: string) (D d) = D (d.Add s)
-    let lookup (s: string) (D d) = Set.exists (fun x -> x = s) d
+    let insert s (D d) = D (d.Add s)
+    let lookup s (D d) = Set.exists (fun x -> x = s) d
